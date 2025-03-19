e1d7ccb1be9e8fea6a0899b8e357c04274cff5c9
Murilo Rodrigues <muriloveroneze0987@gmail.com>
2025-03-19 10:33:21 -0300

    Aula para aprendermos a criar a repetição dos números

---
+++ Aula04/repeticao.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$i = 1;
+
+while ($i <= 6) {
+    echo $i;
+    echo "<br>";
+    $i++;
+}
+
+do {
+    echo $i;
+    $i ++;
+    echo "<br>";
+} while ( $i < 6);
+
+echo "---------<br>";
+
+for ($x = 0; $x <= 10; $x++) {
+    echo "O número é: $x <br>";
+}
+
+
+
+
+
+?>
+<body>
+<html>
