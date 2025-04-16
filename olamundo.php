b67c194c798f074ac4254ad7692ca0efc8a6a8c5
MuriloRVv2 <166238779+MuriloRVv2@users.noreply.github.com>
2025-04-16 19:02:28 -0300

    Add files via upload

---
+++ olamundo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <style>
+        .texto{
+            size: 5rem;  
+        }
+</style>
+<body>
+    <p class="texto">
+        <?php
+            echo "My first PHP script!";
+        ?>
+    </p>
+
+    </body>
+</html>
